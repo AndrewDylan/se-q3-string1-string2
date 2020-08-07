c9011af99d4add7618329813d7e9adf47b71c8b8
Andrew Canter <andrew.canter.5@gmail.com>
2020-08-07 09:08:17 -0400

    passed all tests

--- string1.py
+++ string1.py
@@ -4,7 +4,7 @@ Kenzie assignment: String1
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Andrew Canter"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -28,8 +28,10 @@ __author__ = "???"
 
 
 def donuts(count):
-    # your code here
-    return
+    if count >= 10:
+        return "Number of donuts: many"
+    else:
+        return "Number of donuts: " + str(count)
 
 
 # B. both_ends
@@ -42,8 +44,9 @@ def donuts(count):
 
 
 def both_ends(s):
-    # your code here
-    return
+    if len(s) < 2:
+        return ''
+    return s[0:2] + s[-2:]
 
 
 # C. fix_start
@@ -58,8 +61,7 @@ def both_ends(s):
 
 
 def fix_start(s):
-    # your code here
-    return
+    return s[0] + s[1:].replace(s[0], "*")
 
 
 # D. mix_up
@@ -73,8 +75,10 @@ def fix_start(s):
 
 
 def mix_up(a, b):
-    # your code here
-    return
+    c = a[0:2]
+    a = a.replace(a[0:2], b[0:2], 1)
+    b = b.replace(b[0:2], c, 1)
+    return a + " " + b
 
 
 # Provided simple test() function used in main() to print
